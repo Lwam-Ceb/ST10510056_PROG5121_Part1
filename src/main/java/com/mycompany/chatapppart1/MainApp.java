33b4db3b71eaf95ce1a2c839130d069cf94536e9
Admin <Admin@The-LwaMachine>
2026-05-24 00:41:46 +0200

    Added Part 2 menu and login integration

--- src/main/java/com/mycompany/chatapppart1/MainApp.java
+++ src/main/java/com/mycompany/chatapppart1/MainApp.java
@@ -9,14 +9,16 @@ import java.util.Scanner;
  *
  * @author Student
  */
+//Updated Part 2
 public class MainApp {
       public static void main(String[] args) {
+          
           //Scanner allows for information to be typed in
           Scanner input = new Scanner(System.in);
           //Creating an login class to call it later
           LogIn login = new LogIn();
           
-          // Registation Point
+          // Part 1: Registation Point
           System.out.println("=== USER REGISTRATION ===");
           
           System.out.print("Enter a username ");
@@ -34,7 +36,7 @@ public class MainApp {
           // Shows the registraion message
           System.out.println(response);
           
-          // Login Point
+          // Part 1: Login Point
           System.out.println("\n=== USER LOGIN ===");
           
           System.out.print("Enter a username ");
@@ -50,6 +52,72 @@ public class MainApp {
           String loginMessage = login.returnLoginStatus(loggedIn);
           System.out.println(loginMessage);
           
+         // Open if the user if login is sucessful
+          if (loggedIn) {
+              
+              
+              System.out.println("\nWelcome to ChatApp.");
+              
+              //Lets the user pick a number
+              int choice = 0;
+              
+               //The menu loops till 3 is chosen (Ends loop)
+            while (choice != 3) {
+                
+                //Shows the Menu
+                System.out.println("\n=== CHAT APP MENU ===");
+                
+                System.out.println("1. Send Messages");
+                
+                System.out.println("2. Show Recently Sent Messages");
+                
+                System.out.println("3. Quit");
+                
+                //User selects their choice
+                System.out.print("Choose an option: ");
+                
+                choice = input.nextInt();
+
+                input.nextLine();
+                
+               //Implements a switch option and shows these options based on your choice 
+                switch (choice) {
+                    
+                    case 1:
+                        
+                        //Picking option 1 will show this message
+                        System.out.println("Send Messages selected.");
+                        
+                        break;
+                    
+                    case 2:
+                        
+                        //Picking option 1 will show this message
+                        System.out.println("Recently Sent Messages selected.");
+                        break;
+                    
+                    case 3:
+                        
+                        //Picking option 3 will show this message
+                        System.out.println("Goodbye.");
+                        
+                        break;   
+                        
+                    default:
+                        
+                        //Pick an option not on the menu
+                        System.out.println("Invalid option. Try again.");
+                        
+                }
+            }
+          }else{
+                       //If login failed
+                       System.out.println("Login failed. Exiting application.");
+                       
+          }
+          
+                //Close scanner
+                input.close();
       }
   
 }
